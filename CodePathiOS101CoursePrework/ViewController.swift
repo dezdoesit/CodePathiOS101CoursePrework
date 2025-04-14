3df4755fd036b54128aadd0229a48b4c96a67486
dezdoesit <dez.blair06@gmail.com>
2025-04-14 11:04:18 -0400

    Init Commit + Proj Complete

--- CodePathiOS101CoursePrework/ViewController.swift
+++ CodePathiOS101CoursePrework/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var universityImage: UIImageView!
+    @IBOutlet weak var dreamjobLabel: UILabel!
+    @IBOutlet weak var dreamjobImage: UIImageView!
+    
+    @IBOutlet weak var uniLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Hide labels/images initally
+        uniLabel.isHidden = true
+        universityImage.isHidden = true
+        dreamjobLabel.isHidden = true
+        dreamjobImage.isHidden = true
+        
     }
+    
+    
 
-
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        
+        func changeColor() -> UIColor {
+            
+            let red = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            
+        }
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+    }
+    
+    
+    @IBAction func seeUniversity(_ sender: UIButton) {
+        
+        uniLabel.isHidden = !uniLabel.isHidden
+        universityImage.isHidden = !universityImage.isHidden
+        
+        
+    }
+    
+    @IBAction func seeDreamJob(_ sender: UIButton) {
+        
+        dreamjobLabel.isHidden = !dreamjobLabel.isHidden
+        dreamjobImage.isHidden = !dreamjobImage.isHidden
+        
+        
+    }
+    
+    
+    
 }
 
